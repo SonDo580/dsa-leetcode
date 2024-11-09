9d94b38c49fa7b02e6a638854488f6c09a22a8b9
Son Do <dominhson580@gmail.com>
2024-11-09 17:05:51 +0700

    target sum

---
+++ 1-ArrayAndString/02.target-sum.py
@@ -0,0 +1,23 @@
+# Given a sorted array of unique integers and a target integer,
+# return true if there exists a pair of numbers that sum to target,
+# false otherwise.
+# For example, given nums = [1, 2, 4, 6, 8, 9, 14, 15] and target = 13,
+# return true because 4 + 9 = 13.
+
+# -> use 2-pointers technique
+
+
+def target_sum(nums: list[int], target: int) -> bool:
+    left = 0
+    right = len(nums) - 1
+
+    while left < right:
+        sum = nums[left] + nums[right]
+        if sum == target:
+            return True
+        elif sum > target:
+            right -= 1
+        else:
+            left += 1
+
+    return False
