e908ab11a12e36fb98346d39124428733f38c17b
Son Do <dominhson580@gmail.com>
2024-11-11 00:03:49 +0700

    max sum of subarray with fixed length

---
+++ 1-ArrayAndString/1.2-SlidingWindow/04-subarray-max-sum.py
@@ -0,0 +1,24 @@
+# Given an integer array nums and an integer k,
+# find the sum of the subarray with the largest sum whose length is k.
+
+# => fixed-length window
+
+from typing import List
+
+
+def subarray_max_sum(nums: List[int], k: int) -> int:
+    # build the first window
+    current_sum = 0
+    for i in range(k):
+        current_sum += nums[i]
+
+    # initialize max_sum to the sum of the first window
+    max_sum = current_sum
+
+    # slide the window along the array
+    # (add the right element & remove the left element)
+    for i in range(k, len(nums)):
+        current_sum += nums[i] - nums[i - k]
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
