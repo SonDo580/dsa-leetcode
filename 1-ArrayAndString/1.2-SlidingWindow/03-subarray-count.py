0982bf98dce4055dbfe3f3f74f2ff637ac624ec0
Son Do <dominhson580@gmail.com>
2024-11-10 23:53:06 +0700

    number of subarrays with product less than k

---
+++ 1-ArrayAndString/1.2-SlidingWindow/03-subarray-count.py
@@ -0,0 +1,30 @@
+# Given an array of positive integers nums and an integer k, 
+# return the number of subarrays where the product of all the elements 
+# in the subarray is strictly less than k.
+# 
+# For example, given the input nums = [10, 5, 2, 6], k = 100, 
+# the answer is 8. The subarrays with products less than k are:
+# 
+# [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+
+from typing import List
+
+def subarray_count(nums: List[int], k: int) -> int:
+    if k <= 1:
+        return 0
+    
+    left = 0
+    count = 0
+    current_product = 1
+
+    for right in range(len(nums)):
+        current_product *= nums[right]
+
+        while current_product >= k:
+            current_product //= nums[left]
+            left += 1
+
+        # the (additional) number of valid subarrays is the window length
+        count += right - left + 1    
+
+    return count
