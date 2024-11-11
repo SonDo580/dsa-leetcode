f5a8c06dd95878c939d034eccbe4fa16fd366fa8
Son Do <dominhson580@gmail.com>
2024-11-11 23:46:27 +0700

    max consecutive ones

---
+++ 1-ArrayAndString/1.2-SlidingWindow/06-max-consecutive-ones.py
@@ -0,0 +1,43 @@
+# Given a binary array nums and an integer k, 
+# return the maximum number of consecutive 1's in the array 
+# if you can flip at most k 0's.
+
+# Example 1:
+# Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+# Output: 6
+# Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+#                         x         x
+
+# Example 2:
+# Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+# Output: 10
+# Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+#                       x x       x
+
+# Constraints:
+# 1 <= nums.length <= 105
+# nums[i] is either 0 or 1.
+# 0 <= k <= nums.length
+
+# => Reframe the question:
+# The longest subarray that contains at most k zeros
+
+from typing import List
+
+def longest_ones(nums: List[int], k: int) -> int:
+    left = 0
+    count_zero = 0
+    max_length = 0
+
+    for right in range(len(nums)):
+        if nums[right] == 0:
+            count_zero += 1
+
+        while count_zero > k:
+            if nums[left] == 0:
+                count_zero -= 1
+            left += 1
+        
+        max_length = max(max_length, right - left + 1)
+
+    return max_length
