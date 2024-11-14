65bf2d2696246926b83dfe9e879b304aec288e11
Son Do <dominhson580@gmail.com>
2024-11-14 11:12:38 +0700

    find unique numbers satisfy condition

---
+++ 2-Hashing/2.1-CheckExistence/03.valid-unique-nums.py
@@ -0,0 +1,20 @@
+# Given an integer array nums,
+# find all the unique numbers x in nums that satisfy the following:
+# x + 1 is not in nums, and x - 1 is not in nums.
+
+from typing import List
+
+
+def find_numbers(nums: List[int]) -> List[int]:
+    result = []
+    num_set = set(nums)
+
+    for num in nums:
+        if (num + 1 not in num_set) and (num - 1) not in num_set:
+            result.append(num)
+
+    return result
+
+
+# Time complexity: O(n)
+# Space complexity: O(n)
