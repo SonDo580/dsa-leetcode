24656eab0ec847ecad0ff041f9b15c21a913790d
Son Do <dominhson580@gmail.com>
2024-12-24 00:18:58 +0700

    binary tree - bfs - 2-queue approach

--- 5-TreeAndGraph/5.2-BinaryTreeBFS/00-bfs.py
+++ 5-TreeAndGraph/5.2-BinaryTreeBFS/00-bfs.py
@@ -21,6 +21,36 @@ def bfs(root: TreeNode):
             queue.append(node.right)
 
 
+# ===== Without using built-in deque =====
+#
+# - Use 2 arrays as follow:
+#   + 1 array (queue) for nodes of the current level
+#   + 1 array (next_queue) for nodes of the next level
+#   -> reassign next_queue to queue after processing the current level
+#      (avoid shifting elements when dequeue)
+
+
+def bfs_with_2_queues(root: TreeNode):
+    queue = [root]
+
+    while len(queue) > 0:
+        # Use next_queue to collect nodes of the next level
+        next_queue = []
+
+        for node in queue:
+            # Process current node
+            print(node.val)
+
+            # Add the children to next_queue
+            if node.left:
+                next_queue.append(node.left)
+            if node.right:
+                next_queue.append(node.right)
+
+        # Re-assign next_queue to queue
+        queue = next_queue
+
+
 # ===== Analyze =====
 #
 # Time complexity:
