d999657ae5feada2ffb8e03e9ad3329e76eb446d
Son Do <dominhson580@gmail.com>
2025-01-04 16:24:43 +0700

    graph - check if can visit all rooms

---
+++ 5-TreeAndGraph/5.4-GraphDFS/04.can-visit-all-rooms.py
@@ -0,0 +1,60 @@
+# There are n rooms labeled from 0 to n - 1
+# and all the rooms are locked except for room 0.
+# Your goal is to visit all the rooms.
+# When you visit a room, you may find a set of distinct keys in it.
+# Each key has a number on it, denoting which room it unlocks,
+# and you can take all of them with you to unlock the other rooms.
+# Given an array rooms where rooms[i] is the set of keys
+# that you can obtain if you visited room i,
+# return true if you can visit all the rooms, or false otherwise.
+
+# ===== Analyze =====
+# - the graph is given as an adjacency list
+#   (rooms[i] is an array of other reachable rooms)
+
+# ===== Strategy =====
+# - start a DFS from room 0 and see if we can visit every node
+#   (use a set and check the length, or a boolean array)
+
+from typing import List
+
+
+def can_visit_all_rooms_recursive(rooms: List[List[int]]) -> bool:
+    seen = set()
+
+    def dfs(node):
+        seen.add(node)
+
+        for neighbor in rooms[node]:
+            if neighbor not in seen:
+                seen.add(neighbor)
+                dfs(neighbor)
+
+    dfs(0)
+
+    return len(seen) == len(rooms)
+
+
+def can_visit_all_rooms_iterative(rooms: List[List[int]]) -> bool:
+    seen = set()
+    stack = [0]
+
+    while len(stack) > 0:
+        node = stack.pop()
+        seen.add(node)
+
+        for neighbor in rooms[node]:
+            if neighbor not in seen:
+                stack.append(neighbor)
+
+    return len(seen) == len(rooms)
+
+# ===== Complexity =====
+# 
+# Time complexity: O(n + e)
+# - each node is visited once: O(n)
+# - each edge is processed at most twice: O(e)
+#
+# Space complexity: O(n)
+# - seen: O(n)
+# - recursion call stack: O(n)
