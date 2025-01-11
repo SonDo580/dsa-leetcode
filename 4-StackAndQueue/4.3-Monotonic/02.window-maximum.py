976474499948e7413df44784caa78449fff4fb1b
Son Do <dominhson580@gmail.com>
2025-01-11 17:21:15 +0700

    monotonic - window maximum - add more analysis

--- 4-StackAndQueue/4.3-Monotonic/02.window-maximum.py
+++ 4-StackAndQueue/4.3-Monotonic/02.window-maximum.py
@@ -7,9 +7,20 @@
 # The first window is [|1, 3, -1|, -3, 5, 3, 6, 7] 
 # and the last window is [1, 3, -1, -3, 5, |3, 6, 7|]
 
+# ===== Brute-force approach =====
+# - Loop through all possible starting indices of the window: n - k + 1
+# - For each window, find the maximum value by iterating through its element: k
+# - Append the maximum value to result
+# => Time complexity: O((n - k + 1) * k)
+# 
+# The problem is when the maximum value leaves the window, we do not know
+# what is the second maximum, so we need to search the whole window again.
+
 # ===== Analysis =====
-# - When we see a number, we no longer care about numbers in the window smaller than it,
-#   because they cannot be the maximum
+# - + We need a way to store elements so that if the maximum element is removed,
+#     we know the second maximum, and so forth.
+#   + When we see a number, we no longer care about numbers in the window smaller than it,
+#     because they cannot be the maximum
 #   -> we need a monotonically non-increasing data structure
 #
 # - + Maintain the monotonic property requires removing elements from the right.
