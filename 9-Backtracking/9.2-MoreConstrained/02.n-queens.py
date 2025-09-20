edeff1d19e3546a7997e821975f08517a2197f7a
Son Do <dominhson580@gmail.com>
2025-09-21 00:40:23 +0700

    backtracking - n queens - refine analysis

--- 9-Backtracking/9.2-MoreConstrained/02.n-queens.py
+++ 9-Backtracking/9.2-MoreConstrained/02.n-queens.py
@@ -20,7 +20,21 @@
 # ===== Analysis =====
 # - A queen can attack along the row, column and diagonals it occupies
 #   => We need to place only 1 queen on each row, column, diagonal and anti-diagonal
-# - A brute-force approach would try all n^n placements, which is way too big.
+# 
+# - A brute-force approach would take O(n^n), which is way too big: 
+#   . the first queen has n^2 options
+#   . for each of the first queen options, 
+#     the second queen has n^2 - 1 options
+#   . ...
+#   . for each of the second last queen options, 
+#     the last queen has n^2 - (n - 1) options
+#   -> Number of placements: 
+#        n^2 * (n^2 - 1) * (n^2 - 2) * ... * (n^2 - (n - 1))
+#      = (n^2)! / (n^2 - n)!
+#   -> Complexity: 
+#        O(n^2 * (n^2 - 1) * (n^2 - 2) * ... * (n^2 - (n - 1)))
+#      ~ O((n^2)^n) 
+#      ~ O(n^n)
 
 # ===== Identify backtracking problem ====
 # - We must construct a solution step by step. At each step, there are multiple choices.
@@ -32,13 +46,14 @@
 # - Use occupied columns and diagonals as constraints.
 # - If a queen cannot be placed in the current row, backtrack to the previous row.
 # - A valid solution is found if we can "process" all the rows.
+#   (Since there are n queens and n rows, and each queen needs a different row)
 
 # ===== Implementation =====
 # - Consider 1 row per backtrack call. Pass row as an argument.
 #   When row = n, we found a valid solution.
 # - Use columns / diagonals / anti-diagonals as constraints.
 #   Use 3 sets to track which column / diagonal / anti-diagonal has been occupied.
-#   When we place a queen, add column / diagonal / anti-diagonal from the set.
+#   When we place a queen, add column / diagonal / anti-diagonal to the set.
 #   When we backtrack and remove a queen, remove column / diagonal / anti-diagonal from the set.
 # - All squares in the same diagonal has the same row - col.
 #   Diagonal move down and right, so both row and col get incremented.
