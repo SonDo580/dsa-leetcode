483978bd069c22e5528587125d20be8f57545de3
Son Do <dominhson580@gmail.com>
2025-09-20 23:09:36 +0700

    backtracking - permutations

--- 9-Backtracking/9.1-Generation/01.permutations.py
+++ 9-Backtracking/9.1-Generation/01.permutations.py
@@ -19,47 +19,92 @@
 # -10 <= nums[i] <= 10
 # All the integers of nums are unique.
 
-# ===== Analyze =====
-# To build all permutations, we can select all elements at first index.
-# For the second index, we can select all remaining elements. And so on.
+
+# ===== Strategy =====
+# - To build all permutations, we can have n choices for the first position.
+# - For the second position, we can n - 1 remaining choices. And so on.
 # => Number of permutations: n * (n - 1) * ... * 1 = n!
 
 
-# ===== Recursive divide-and-conquer =====
 def get_permutations(nums: list[int]) -> list[list[int]]:
-    # 1 element -> 1 permutation
+    # Base case: 1 element -> 1 permutation
     if len(nums) == 1:
         return [nums]
 
-    result: list[list[int]] = []  # store all possible permutations
+    all_permutations: list[list[int]] = []
 
     for i in range(len(nums)):
-        # find all permutations of remaining numbers
-        rest_permutations = get_permutations(nums[:i] + nums[(i + 1):])
+        # Find all permutations of remaining numbers (exclude current number)
+        rest_permutations = get_permutations(nums[:i] + nums[(i + 1) :])
 
-        # combine current number with permutations of remaining numbers
+        # Combine current number with permutations of remaining numbers
         for permutation in rest_permutations:
-            result.append([nums[i]] + permutation)
-    
-    return result
+            all_permutations.append([nums[i]] + permutation)
 
-# Time complexity:
+    return all_permutations
+
+
+# ===== Complexity =====
+# 1. Time complexity:
 # Let T(n) be the time complexity for an input of size n
 # - Iterate through the list of numbers: O(n)
 # - In each iteration:
 #   + creating a new list without the current element: O(n)
 #   + recursively call get_permutations on the new list: T(n - 1)
-#   + combining result: O((n-1)!) (rest_permutations size)
-# => T(n) = O(n^2) + O(n)*T(n-1) + O(n(n-1)!)
-#         = O(n!) + O(n)*T(n-1) 
-# - O(n)*T(n-1) = O(n) * O(n - 1) * O(n - 2) * ... * T(1)
+#   + combining result: O((n - 1)!) (rest_permutations size)
+# => T(n) = O(n) * (O(n) + T(n - 1) + O((n - 1)!))
+#         = O(n^2) + O(n) * T(n - 1) + O(n * (n - 1)!)
+#         = O(n!) + O(n) * T(n - 1)
+# . T(n - 1) = O((n - 1)!) + O(n - 1) * T(n - 2)
+#   T(n - 2) = O((n - 2)!) + O(n - 2) * T(n - 3)
+#   ...
 #   T(1) = O(1) (return the result right away)
-# => T(n) = O(n!)
-  
-# Space complexity: O(n!)
-# - result array: 
+# . O(n) * T(n - 1) = O(n) * O((n - 1)!) +
+#                     O(n) * O(n - 1) * T(n - 2)
+#                   = O(n!) +
+#                     O(n * (n - 1)) * O((n - 2)!) +
+#                     O(n) * O(n - 1) * O(n - 2) * T(n - 3)
+#                   = ...
+#                   = (n - 1) * O(n!) +
+#                     O(n) * O(n - 1) * O(n - 2) * ... * T(1)
+#                   = (n - 1) * O(n!) +
+#                     O(n) * O(n - 1) * O(n - 2) * ... * O(1)
+#                   = O(n * n!)
+# => T(n) = O(n!) + O(n * n!) = O(n * n!)
+#
+# 2. Space complexity:
+# - result array: O(n * n!)
 #   + number of permutations is n!
 #   + each permutation has length n
-#   -> O(n*n!) = O(n!) 
 # - recursion stack: O(n)
 #   (the function recursively calls itself until n == 1)
+# => Overall: O(n * n!)
+
+
+# ===== Approach 2 =====
+# - Build each permutation using a recursive function backtrack(current),
+#   where current is the permutation being built.
+# - The base case is when current.length = nums.length (the permutation is completed).
+#   -> add 'current' to the result list
+# - To build all permutations, we need all elements at the first index. and for each of those elements,
+#   we need all the other element at the second index, and so on.
+#   -> each 'backtrack' call needs to loop through all elements,
+#      except for those that have been used in 'current'.
+def get_permutations(nums: list[int]) -> list[list[int]]:
+    all_permutations: list[list[int]] = []
+
+    def backtrack(current: list[int]):
+        if len(current) == len(nums):
+            # - We mutate 'current' across 'backtrack' calls,
+            #   -> create a copy of 'current' when adding
+            all_permutations.append(current[:])
+            return
+
+        for num in nums:
+            if num not in current:
+                current.append(num)
+                backtrack(current)
+                current.pop()
+
+    backtrack([])
+    return all_permutations
