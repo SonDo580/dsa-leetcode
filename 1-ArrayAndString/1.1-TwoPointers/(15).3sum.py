24b2980e25c6e048a59af84c367071d1a547cac5
Son Do <dominhson580@gmail.com>
2026-06-25 10:44:19 +0700

    2 pointers: 3sum - improve & fix complexity analysis

--- 1-ArrayAndString/1.1-TwoPointers/(15).3sum.py
+++ 1-ArrayAndString/1.1-TwoPointers/(15).3sum.py
@@ -11,8 +11,7 @@ Notice that the solution set must not contain duplicate triplets.
 
 """
 Idea:
-- Keep 1 item fixed
-  -> Finding the other 2 items is 'two-sum' problem.
+- Keep 1 item fixed, then finding the other 2 items is '2sum' problem.
 - To use '2 pointers', 'nums' must be sorted.
   We only need to return values -> don't need to track original indices.
 """
@@ -28,11 +27,9 @@ def three_sum(nums: list[int]) -> list[list[int]]:
         if nums[i] > 0:
             # nums[i] <= nums[j] <= nums[k]
             # -> nums[i] + nums[j] + nums[k] >= 3 * nums[i]
-            # nums[i] > 0
-            # -> nums[i] + nums[j] + nums[k] > 0
+            # -> nums[i] + nums[j] + nums[k] > 3 * 0 (cannot == 0)
             break
 
-        # 2-sum problem
         j = i + 1
         k = n - 1
         while j < k:
@@ -41,12 +38,15 @@ def three_sum(nums: list[int]) -> list[list[int]]:
                 ans.append([nums[i], nums[j], nums[k]])
 
                 # skip duplicate nums[j]
-                while j < n - 1 and nums[j + 1] == nums[j]:
+                while j < k and nums[j + 1] == nums[j]:
                     j += 1
-                j += 1  # increase nums[j]
+                
+                # increase nums[j] OR cause j > k
+                # (staying the same only results in duplicate pairs)
+                j += 1  
 
                 # duplicate nums[k] will be skipped in subsequent iterations,
-                # since it results in sum3 > 0 after nums[j] has increased.
+                # since it results in sum3 > 0 after nums[j] has increased
                 k -= 1
             elif sum3 < 0:
                 j += 1
@@ -56,7 +56,7 @@ def three_sum(nums: list[int]) -> list[list[int]]:
         # skip duplicate nums[i]
         while i < n - 1 and nums[i + 1] == nums[i]:
             i += 1
-        i += 1  # increase nums[i]
+        i += 1  # increase nums[i] OR cause i > n - 1
 
     return ans
 
@@ -64,6 +64,11 @@ def three_sum(nums: list[int]) -> list[list[int]]:
 """
 Complexity:
 - Let n = len(nums)
+
 1. Time complexity: O(n^2)
-2. Space complexity: O(1)
+- sort 'nums': O(n*log(n)) (timsort)
+- main loop: O(n^2)
+
+2. Space complexity: O(n)
+- sorting: O(n) (timsort)
 """
