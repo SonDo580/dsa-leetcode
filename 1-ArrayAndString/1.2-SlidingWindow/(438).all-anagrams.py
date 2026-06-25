9365eefb8fa950c751d7ddcd52a1d0afb32e0e82
Son Do <dominhson580@gmail.com>
2026-06-25 11:03:01 +0700

    sliding window: all anagrams

--- 1-ArrayAndString/1.2-SlidingWindow/(438).all-anagrams.py
+++ 1-ArrayAndString/1.2-SlidingWindow/(438).all-anagrams.py
@@ -1,7 +1,8 @@
 """
 https://leetcode.com/problems/find-all-anagrams-in-a-string/
 
-Given two strings s and p, return an array of all the start indices of p's anagrams in s.
+Given two strings s and p, 
+return an array of all the start indices of p's anagrams in s.
 You may return the answer in any order.
 
 An anagram is a word or phrase formed by
@@ -14,26 +15,19 @@ Idea: Use sliding window
 - Build the character frequency dictionary for p (cnt).
 - Slide window:
   . cnt[c]-- when adding c to window (extend right).
-  . cnt[c]++ when removing c to window (shrink left).
-- Maintain constraint: all(cnt[c] >= 0)
+  . cnt[c]++ when removing c from window (shrink left).
+- Maintain constraint: all(cnt[c] == 0)
+  . initially, all cnt[c] > 0
+  . extending right: reduce 1 cnt[c].
   . cnt[c] < 0
     -> the window contains exceeding c
     -> shrink left until cnt[c] == 0
   . c not in cnt
-    -> results in cnt[c] < 0 (depends on implementation)
-- After that, if all(cnt[c] == 0), [left..right] forms a valid anagram of p
+    -> results in cnt[c] < 0
+    -> also shrink left until cnt[c] == 0
+- After that, if right-left+1 == len(p)
+  -> [left..right] forms a valid anagram of p
   -> record left.
-- Faster check: right-left+1 (window length) == len(p). 
-  WHY:
-  . Consider a valid window with length=len(p) 
-  . After the maintaining, there's no cnt[c] < 0 .
-    (no c that is not in p, no exceeding c) 
-  . There's no cnt[c] > 0 either.
-    Assume there's some characters with cnt[c] > 0 (lack c),
-    The remaining characters have cnt[c'] == 0 (enough c').
-    Then we must use some of those c' to fill up len(p),
-    which makes some cnt[c'] < 0 -> CONTRADICT 
-  -> all cnt[c] == 0
 """
 
 from collections import defaultdict
