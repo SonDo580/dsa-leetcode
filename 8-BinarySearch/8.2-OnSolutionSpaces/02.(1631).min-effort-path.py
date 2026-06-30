80645e4c3c0c82c84a6c99bba424824634b3cf7a
Son Do <dominhson580@gmail.com>
2026-06-30 23:10:44 +0700

    binary search - on solution space: refine notes

--- 8-BinarySearch/8.2-OnSolutionSpaces/02.(1631).min-effort-path.py
+++ 8-BinarySearch/8.2-OnSolutionSpaces/02.(1631).min-effort-path.py
@@ -17,39 +17,6 @@ between two consecutive cells of the route.
 
 Return the minimum effort required to travel
 from the top-left cell to the bottom-right cell.
-
-Example 1:
-Input: heights =
-[[1,2,2],
- [3,8,2],
- [5,3,5]]
-Output: 2
-Explanation: The route of [1,3,5,3,5] has a maximum absolute difference of 2 in consecutive cells.
-This is better than the route of [1,2,2,2,5], where the maximum absolute difference is 3.
-
-Example 2:
-Input: heights =
-[[1,2,3],
- [3,8,4],
- [5,3,5]]
-Output: 1
-Explanation: The route of [1,2,3,4,5] has a maximum absolute difference of 1 in consecutive cells, which is better than route [1,3,5,3,5].
-
-Example 3:
-Input: heights =
-[[1,2,1,1,1],
- [1,2,1,2,1],
- [1,2,1,2,1],
- [1,2,1,2,1],
- [1,1,1,2,1]]
-Output: 0
-Explanation: This route does not require any effort.
-
-Constraints:
-rows == heights.length
-columns == heights[i].length
-1 <= rows, columns <= 100
-1 <= heights[i][j] <= 10^6
 """
 
 """
@@ -59,7 +26,7 @@ Analysis:
   and any value less than it is impossible.
 
 - Given an effort, we need to check if a valid path exists.
-  + Perform DFS starting from 0, with edges are the 4 directions and
+  + Perform DFS starting from (0, 0), with edges are the 4 directions and
     only traversable if difference <= effort.
   + Also check if cell is valid and hasn't been visited.
     (There's already a valid path up to that cell, so don't visit it again)
--- 8-BinarySearch/8.2-OnSolutionSpaces/03.(1870).min-train-speed.py
+++ 8-BinarySearch/8.2-OnSolutionSpaces/03.(1870).min-train-speed.py
@@ -10,7 +10,8 @@ where dist[i] describes the distance (in kilometers) of the ith train ride.
 Each train can only depart at an integer hour,
 so you may need to wait in between each train ride.
 For example, if the 1st train ride takes 1.5 hours,
-you must wait for an additional 0.5 hours before you can depart on the 2nd train ride at the 2 hour mark.
+you must wait for an additional 0.5 hours before
+you can depart on the 2nd train ride at the 2 hour mark.
 
 Return the minimum positive integer speed (in kilometers per hour)
 that all the trains must travel at for you to reach the office on time,
@@ -18,36 +19,6 @@ or -1 if it is impossible to be on time.
 
 Tests are generated such that the answer will not exceed 10^7
 and hour will have at most two digits after the decimal point.
-
-Example 1:
-Input: dist = [1,3,2], hour = 6
-Output: 1
-Explanation: At speed 1:
-- The first train ride takes 1/1 = 1 hour.
-- Since we are already at an integer hour, we depart immediately at the 1 hour mark. The second train takes 3/1 = 3 hours.
-- Since we are already at an integer hour, we depart immediately at the 4 hour mark. The third train takes 2/1 = 2 hours.
-- You will arrive at exactly the 6 hour mark.
-
-Example 2:
-Input: dist = [1,3,2], hour = 2.7
-Output: 3
-Explanation: At speed 3:
-- The first train ride takes 1/3 = 0.33333 hours.
-- Since we are not at an integer hour, we wait until the 1 hour mark to depart. The second train ride takes 3/3 = 1 hour.
-- Since we are already at an integer hour, we depart immediately at the 2 hour mark. The third train takes 2/3 = 0.66667 hours.
-- You will arrive at the 2.66667 hour mark.
-
-Example 3:
-Input: dist = [1,3,2], hour = 1.9
-Output: -1
-Explanation: It is impossible because the earliest the third train can depart is at the 2 hour mark.
-
-Constraints:
-n == dist.length
-1 <= n <= 10^5
-1 <= dist[i] <= 10^5
-1 <= hour <= 10^9
-There will be at most two digits after the decimal point in hour.
 """
 
 """
@@ -66,7 +37,7 @@ Analysis:
 - Since each train ride takes at least 1 hour, 
   if there are more trains than hours allowed,
   it is impossible to find an answer.
-- The last train can take fractional time (no more waiting),
+  The last train can take fractional time (no more waiting),
   so we should compare number of trains with hours rounded up.
 """
 
@@ -83,6 +54,7 @@ def min_train_speed(dist: list[int], hour: float) -> int:
         for distance in dist:
             time = math.ceil(time)
             time += distance / speed
+            # the last time is not rounded up
         return time <= hour
 
     left = 1
