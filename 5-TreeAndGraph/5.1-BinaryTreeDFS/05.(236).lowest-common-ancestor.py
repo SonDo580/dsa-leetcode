a99a62d5e98f10954518ae34aa6c9b02343f7cc2
Son Do <dominhson580@gmail.com>
2026-07-08 10:39:48 +0700

    tree DFS: lowest common ancestor - extra approach

--- 5-TreeAndGraph/5.1-BinaryTreeDFS/05.(236).lowest-common-ancestor.py
+++ 5-TreeAndGraph/5.1-BinaryTreeDFS/05.(236).lowest-common-ancestor.py
@@ -30,7 +30,7 @@ Possibilities:
 """
 
 
-def lowest_common_ancestor_recur(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
     def _dfs(node: TreeNode | None) -> TreeNode | None:
         """
         Return the lowest ancestor of p or q in a subtree.
@@ -80,3 +80,55 @@ Complexity:
 1. Time complexity: O(n) (each node is processed once)
 2. Space complexity: O(h) for recursion stack 
 """
+
+
+# === Another approach ===
+"""
+- For a node to be a common ancestor of p and q:
+  . node is an ancestor of p
+  . AND node is an ancestor of q
+- For a node to be an ancestor of p:
+  . node == p
+  . OR node.left is an ancestor of p
+  . OR node.right is an ancestor of p
+  (Similar for q)
+
+=> Use DFS (post-order traversal) to calculate result of a node
+   based on results from its children.
+   . Return: (is_ancestor_of_p, is_ancestor_of_q)
+   . The lowest common ancestor is the 1st common ancestor found
+     through DFS (lower nodes are processed before higher nodes).  
+"""
+
+
+def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+    ans = None
+
+    def is_ancestor_of_pq(node: TreeNode | None) -> tuple[bool, bool]:
+        """Return (is_node_ancestor_of_p, is_node_ancestor_of_q)."""
+        nonlocal ans
+
+        # # Optional optimization
+        # if ans:  # already found the valid answer
+        #     return False, False  # dummy values (not used by higher calls)
+
+        if not node:
+            return False, False
+
+        is_left_ancestor_of_p, is_left_ancestor_of_q = is_ancestor_of_pq(node.left)
+        is_right_ancestor_of_p, is_right_ancestor_of_q = is_ancestor_of_pq(node.right)
+        is_node_ancestor_of_p = (
+            node == p or is_left_ancestor_of_p or is_right_ancestor_of_p
+        )
+        is_node_ancestor_of_q = (
+            node == q or is_left_ancestor_of_q or is_right_ancestor_of_q
+        )
+
+        # Only record the 1st common ancestor encountered (lowest)
+        if not ans and is_node_ancestor_of_p and is_node_ancestor_of_q:
+            ans = node
+
+        return is_node_ancestor_of_p, is_node_ancestor_of_q
+
+    is_ancestor_of_pq(root)
+    return ans
