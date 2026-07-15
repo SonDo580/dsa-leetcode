373da01008027b86eaa694c6d1d8d5da6efeb7c1
Son Do <dominhson580@gmail.com>
2026-07-15 11:37:41 +0700

    find city: dijkstra

--- 13-GraphExtra/13.4-ShortestWeightedPath/(1334).find-the-city.py
+++ 13-GraphExtra/13.4-ShortestWeightedPath/(1334).find-the-city.py
@@ -14,10 +14,9 @@ Notice that the distance of a path connecting cities i and j
 is equal to the sum of the edges' weights along that path.
 """
 
+# === Approach 1: Floyd-Warshall ===
 """
-Idea:
 - Find the shortest distance between all (i, j) pairs.
-  Use Floyd-Warshall algorithm.
 - If d[i][j] < distanceThreshold,
   increase the number of reachable cities for both i and j.
 - Return the city with the minimum number of reachable cities.
@@ -27,19 +26,9 @@ Idea:
 import math
 
 
-def findTheCity(n: int, edges: list[list[int]], distance_threshold: int) -> int:
-    # Find the shortest distance between all (i, j) pairs
-    d: list[list[int | float]] = [[math.inf] * n for _ in range(n)]
-    for i in range(n):
-        d[i][i] = 0
-    for u, v, weight in edges:
-        # the edges are bidirectional
-        d[u][v] = weight
-        d[v][u] = weight
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                d[i][j] = min(d[i][j], d[i][k - 1] + d[k - 1][j])
+def find_the_city(n: int, edges: list[list[int]], distance_threshold: int) -> int:
+    # Find the shortest distance between all pairs of nodes
+    d = _floyd_warshall(n, edges)
 
     # Find the number of reachable cities from each city
     reachable: list[int] = [0] * n
@@ -60,6 +49,25 @@ def findTheCity(n: int, edges: list[list[int]], distance_threshold: int) -> int:
     return city_with_min_reachable
 
 
+def _floyd_warshall(n: int, edges: list[list[int]]) -> list[list[int | float]]:
+    """Find the shortest distance between all pairs of nodes"""
+    d: list[list[int | float]] = [[math.inf] * n for _ in range(n)]
+
+    for i in range(n):
+        d[i][i] = 0
+    for u, v, weight in edges:
+        # the edges are bidirectional
+        d[u][v] = weight
+        d[v][u] = weight
+
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                d[i][j] = min(d[i][j], d[i][k - 1] + d[k - 1][j])
+
+    return d
+
+
 """
 Complexity:
 - Let V = n             (number of vertices)
@@ -72,5 +80,86 @@ Complexity:
 
 2. Space complexity: O(V^2)
 - 'd': O(V^2)
+- 'reachable': O(V)
+"""
+
+# === Approach 2: Dijkstra ===
+"""
+- Constraints: 1 <= weight_i <= 10^4
+  -> no negative-weight edges
+  -> use Dijkstra starting from each node.
+"""
+
+from heapq import heappush, heappop
+from collections import defaultdict
+
+
+def find_the_city(n: int, edges: list[list[int]], distance_threshold: int) -> int:
+    # Build adjacency list
+    adj_list: defaultdict[int, list[tuple[int, int]]] = defaultdict(list)
+    for u, v, weight in edges:
+        # the edges are bidirectional
+        adj_list[u].append((v, weight))
+        adj_list[v].append((u, weight))
+
+    # Find the shortest distance between all pairs of nodes
+    d: list[list[int | float]] = []
+    for i in range(n):
+        d.append(_dijkstra(n, i, adj_list))
+
+    # Find the number of reachable cities from each city
+    reachable: list[int] = [0] * n
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if d[i][j] <= distance_threshold:
+                reachable[i] += 1
+                reachable[j] += 1
+
+    # Find greatest-label city with minimum number of reachable cities
+    min_reachable = math.inf
+    city_with_min_reachable = -1
+    for i in range(n):
+        if reachable[i] <= min_reachable:
+            min_reachable = reachable[i]
+            city_with_min_reachable = i
+
+    return city_with_min_reachable
+
+
+def _dijkstra(
+    n: int, source: int, adj_list: defaultdict[int, list[tuple[int, int]]]
+) -> list[int | float]:
+    d = [math.inf] * n
+    heap: list[tuple[int, int]] = []
+    d[source] = 0
+    heappush(heap, (0, source))
+
+    while heap:
+        du, u = heappop(heap)
+        if du > d[u]:
+            continue
+
+        for v, weight in adj_list[u]:
+            if du + weight < d[v]:
+                d[v] = du + weight
+                heappush(heap, (d[v], v))
+
+    return d
+
+
+"""
+Complexity:
+
+1. Time complexity: O(V^2 + V*E*log(E))
+- Build adjacency list: O(E)
+- Perform Dijkstra for all nodes: O(V^2 + V*E*log(E))
+  . each Dijkstra: O(V + E*log(E))
+- Calculate 'reachable': O(V^2)
+- Find answer: O(V)
+
+2. Space complexity: O(V^2 + E)
+- adjacency list: O(V + E)
+- 'd': O(V^2) (include all return values of Dijkstra)
+- Dijkstra's heap: O(E)
 - 'reachable': O(V)
 """
