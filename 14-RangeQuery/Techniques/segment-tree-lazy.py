e5460b57576fe2513236b9f8a0e0fe3f923950a5
Son Do <dominhson580@gmail.com>
2026-07-19 16:37:49 +0700

    segment tree: lazy propagation - alternative implementation

--- 14-RangeQuery/Techniques/segment-tree-lazy.py
+++ 14-RangeQuery/Techniques/segment-tree-lazy.py
@@ -1,22 +1,19 @@
-class SegmentTreeLazy:
-    """
-    Segment tree for range sum query with range updates.
-    Use lazy propagation.
-    """
+from abc import ABC, abstractmethod
 
+
+class BaseSegmentTreeLazy(ABC):
     def __init__(self, nums: list[int]):
         n = len(nums)
         self.tree = [0] * 4 * n
         self.__build_tree(nums, left=0, right=n - 1, i=0)
 
-        # Store pending updates
-        # (value to add to each element in the range of each node)
+        # pending updates (value to add to each element in node's range)
         self.lazy = [0] * 4 * n
 
-    def __lci(self, i: int) -> int:
+    def _lci(self, i: int) -> int:
         return 2 * i + 1
 
-    def __rci(self, i: int) -> int:
+    def _rci(self, i: int) -> int:
         return 2 * i + 2
 
     def __build_tree(self, nums: list[int], left: int, right: int, i: int) -> None:
@@ -26,30 +23,51 @@ class SegmentTreeLazy:
             return
 
         mid = (left + right) // 2
-        lci = self.__lci(i)
-        rci = self.__rci(i)
+        lci = self._lci(i)
+        rci = self._rci(i)
         self.__build_tree(nums, left=left, right=mid, i=lci)  # build left subtree
         self.__build_tree(nums, left=mid + 1, right=right, i=rci)  # build right subtree
         self.tree[i] = self.tree[lci] + self.tree[rci]  # store aggregated result
 
-    def __push_update(self, left: int, right: int, i: int) -> None:
-        """Propagate pending update to direct children of node i."""
-        if self.lazy[i] == 0:
-            # no pending update
+    @abstractmethod
+    def update_range(
+        self, qleft: int, qright: int, inc: int, left: int, right: int, i: int
+    ) -> None: ...
+
+    @abstractmethod
+    def query(self, qleft: int, qright: int, left: int, right: int, i: int) -> int: ...
+
+
+class SegmentTreeLazyV1(BaseSegmentTreeLazy):
+    """
+    Segment tree for range sum query with range updates.
+    Use lazy propagation (v1).
+    """
+
+    def __process_pending(self, left: int, right: int, i: int) -> None:
+        """
+        Propagate pending update to direct children of node i
+        (Node i has been updated)."""
+        # process_pending is never called on a leaf node
+        # - only called when node's range partially overlaps with query/update range
+        # - a leaf's range (single point) is either outside or inside query/update range 
+        assert left < right
+
+        # no pending update
+        if self.lazy[i] == 0:  
             return
 
-        mid = (left + right) // 2
-        lci = self.__lci(i)
-        rci = self.__rci(i)
-
-        # Apply update to direct children
+        # apply update to direct children
         inc = self.lazy[i]
+        mid = (left + right) // 2
+        lci = self._lci(i)
+        rci = self._rci(i)
         self.lazy[lci] += inc
         self.lazy[rci] += inc
         self.tree[lci] += inc * (mid - left + 1)
         self.tree[rci] += inc * (right - mid)
 
-        # Reset lazy state for current node
+        # reset pending state for current node
         self.lazy[i] = 0
 
     def update_range(
@@ -68,12 +86,12 @@ class SegmentTreeLazy:
             return
 
         # partial overlap: push pending update down to ensure children have correct values
-        self.__push_update(left, right, i)
+        self.__process_pending(left, right, i)
 
         # update children
         mid = (left + right) // 2
-        lci = self.__lci(i)
-        rci = self.__rci(i)
+        lci = self._lci(i)
+        rci = self._rci(i)
         self.update_range(qleft, qright, inc, left=left, right=mid, i=lci)
         self.update_range(qleft, qright, inc, left=mid + 1, right=right, i=rci)
 
@@ -92,12 +110,100 @@ class SegmentTreeLazy:
             return self.tree[i]
 
         # partial overlap: push pending update down to ensure children have correct values
-        self.__push_update(left, right, i)
+        self.__process_pending(left, right, i)
 
+        # aggregate result from both halves
         mid = (left + right) // 2
-        left_result = self.query(qleft, qright, left=left, right=mid, i=self.__lci(i))
+        left_result = self.query(qleft, qright, left=left, right=mid, i=self._lci(i))
         right_result = self.query(
-            qleft, qright, left=mid + 1, right=right, i=self.__rci(i)
+            qleft, qright, left=mid + 1, right=right, i=self._rci(i)
+        )
+        return left_result + right_result
+
+
+class SegmentTreeLazyV2(BaseSegmentTreeLazy):
+    """
+    Segment tree for range sum query with range updates.
+    Use lazy propagation (v2).
+    """
+
+    def __process_pending(self, left: int, right: int, i: int) -> None:
+        """
+        Apply pending update to node i and
+        propagate pending update to its direct children.
+        """
+        if self.lazy[i] == 0:
+            # no pending update
+            return
+
+        # apply pending update to node i
+        inc = self.lazy[i]
+        self.tree[i] += inc * (right - left + 1)
+
+        # reset pending state for node i
+        self.lazy[i] = 0
+
+        if left < right:  # node i is not a leaf
+            # propagate pending update to direct children
+            lci = self._lci(i)
+            rci = self._rci(i)
+            self.lazy[lci] += inc
+            self.lazy[rci] += inc
+
+    def update_range(
+        self, qleft: int, qright: int, inc: int, left: int, right: int, i: int
+    ) -> None:
+        """Add 'inc' to all elements in range [qleft, qright]."""
+        # query range is outside node's range -> skip
+        if qright < left or qleft > right:
+            return
+
+        # node's range is completely inside query range
+        if qleft <= left and right <= qright:
+            # save pending update
+            self.lazy[i] += inc
+            return
+
+        # === partial overlap ===
+
+        # apply pending update to current node and
+        # propagate pending update to direct children
+        self.__process_pending(left, right, i)
+
+        # update children
+        mid = (left + right) // 2
+        lci = self._lci(i)
+        rci = self._rci(i)
+        self.update_range(qleft, qright, inc, left=left, right=mid, i=lci)
+        self.update_range(qleft, qright, inc, left=mid + 1, right=right, i=rci)
+
+        # update current node after updating children
+        self.tree[i] = self.tree[lci] + self.tree[rci]
+
+    def query(self, qleft: int, qright: int, left: int, right: int, i: int) -> int:
+        """Return aggregated result for the range [qleft, qright]."""
+        # query range is outside node's range -> return 0
+        if qright < left or qleft > right:
+            return 0
+
+        # node's range is completely inside query range
+        # -> return value of the node
+        if qleft <= left and right <= qright:
+            # apply pending update before reading node value
+            self.__process_pending(left, right, i)
+            return self.tree[i]
+
+        # === partial overlap ===
+
+        # apply pending update to current node and
+        # propagate pending update to direct children
+        self.__process_pending(left, right, i)
+
+        # aggregate result from both halves
+        mid = (left + right) // 2
+        left_result = self.query(qleft, qright, left=left, right=mid, i=self._lci(i))
+        right_result = self.query(
+            qleft, qright, left=mid + 1, right=right, i=self._rci(i)
         )
         return left_result + right_result
 
@@ -109,7 +215,7 @@ Complexity:
 
 1. Time complexity:
 - Build tree: O(4*n) = O(n)
-- Push update: O(1)
+- Process pending update: O(1)
 - Query: O(log(n))
   . If node's range is completely outside OR fully contained in query range 
     -> Return immediately
@@ -127,18 +233,22 @@ Complexity:
 
 
 # ===== Quick test =====
-nums = [1, 1, 1, 1, 1]
-n = len(nums)
-st = SegmentTreeLazy(nums)
+from typing import Type
 
-assert st.query(qleft=0, qright=3, left=0, right=n - 1, i=0) == 1 + 1 + 1 + 1
+StClasses: list[Type[BaseSegmentTreeLazy]] = [SegmentTreeLazyV1, SegmentTreeLazyV2]
+for StClass in StClasses:
+    nums = [1, 1, 1, 1, 1]
+    n = len(nums)
+    st = StClass(nums)
 
-# Add 10 to nums[0..2]
-for i in range(3):
-    nums[i] += 10
-st.update_range(qleft=0, qright=2, inc=10, left=0, right=n - 1, i=0)
-assert nums == [11, 11, 11, 1, 1]
+    assert st.query(qleft=0, qright=3, left=0, right=n - 1, i=0) == 1 + 1 + 1 + 1
 
-assert st.query(qleft=0, qright=3, left=0, right=n - 1, i=0) == 11 + 11 + 11 + 1
+    # Add 10 to nums[0..2]
+    for i in range(3):
+        nums[i] += 10
+    st.update_range(qleft=0, qright=2, inc=10, left=0, right=n - 1, i=0)
+    assert nums == [11, 11, 11, 1, 1]
 
-print("All tests passed")
+    assert st.query(qleft=0, qright=3, left=0, right=n - 1, i=0) == 11 + 11 + 11 + 1
+
+    print(f"All {StClass.__name__} tests passed")
